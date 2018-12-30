a05b91f24938cdd194f03af12f8000511a3ca1a2
Adrien Burgun <adrien.burgun@orange.fr>
2018-12-30 14:27:46 +0100

    Implemented Commands, CommandBuilders and CommandResults (untested).

--- index.js
+++ index.js
@@ -6,3 +6,7 @@
  * Licensed under the MIT license. For more information,
  * see the LICENSE file.
  */
+
+module.exports.Command = require("./lib/command");
+module.exports.CommandResult = require("./lib/commandResult");
+module.exports.CommandBuilder = require("./lib/commandBuilder");
