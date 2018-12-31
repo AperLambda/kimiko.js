efee219a660e16b303083b882d4e59cf754eb098
Adrien Burgun <adrien.burgun@orange.fr>
2018-12-31 11:37:42 +0100

    :sparkles: :art: ResourceNames and `} else`s (man I hate these)
    Note that `ResourceName` implementations are as transparent as possible,
    this means that:
    
    let rn = new kimiko.ResourceName("a:test");
    console.log(rn == "a:test"); // true

--- index.js
+++ index.js
@@ -11,3 +11,4 @@ module.exports.Command = require("./lib/Command");
 module.exports.CommandResult = require("./lib/CommandResult");
 module.exports.CommandBuilder = require("./lib/CommandBuilder");
 module.exports.CommandManager = require("./lib/CommandManager");
+module.exports.ResourceName = require("./lib/ResourceName");
