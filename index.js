d20c2dc08a557c212285128bc3c0bf72f996c25c
LambdAurora <aurora42lambda@gmail.com>
2020-03-19 15:23:25 +0100

    :boom: Rewrite everything.

--- index.js
+++ index.js
@@ -7,8 +7,18 @@
  * see the LICENSE file.
  */
 
-module.exports.Command = require("./lib/Command");
+import * as kimiko from "./lib/kimiko.mjs";
+import {parse_command_syntax, parse_arguments} from "./lib/command_parser.mjs";
+
+/*module.exports.Command = require("./lib/Command");
 module.exports.CommandResult = require("./lib/CommandResult");
 module.exports.CommandBuilder = require("./lib/CommandBuilder");
-module.exports.CommandManager = require("./lib/CommandManager");
-module.exports.ResourceName = require("./lib/ResourceName");
+module.exports.CommandManager = require("./lib/CommandManager");*/
+
+let test_command_syntax = "chloe:remindme <event> <date> [repeat:boolean] [users:int...]";
+let parsed_cmd;
+console.log(test_command_syntax);
+console.log(parsed_cmd = parse_command_syntax(test_command_syntax));
+let cmd = new kimiko.Command(parsed_cmd.name);
+cmd.arguments = parsed_cmd.args;
+console.log(parse_arguments(cmd, '"this is an event" 23:35:20 yes 5050 0o56'))
